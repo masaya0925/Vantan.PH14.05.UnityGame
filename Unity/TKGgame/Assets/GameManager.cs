f625bfa23a46077f195e9dd54228d882b6f3718c
masaya0925 <masaya_suda0925@i.softbank.jp>
2016-07-07 10:43:28 +0900

    UnityProjectを追加

---
+++ Unity/TKGgame/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour {
+	public Text _text;
+	public bool _onetouch = false;
+	public bool _clear = false;
+	public EggController egg;
+
+	void Update () {
+		if (!_onetouch) {
+			if (Input.GetMouseButton (0)) {
+				var screenPoint = Input.mousePosition;
+				    screenPoint.z = 18;
+				    screenPoint.y = 320;
+				var screenToWorld = Camera.main.ScreenToWorldPoint (screenPoint);
+				egg.SetPosition(screenToWorld);
+
+			} else if (Input.GetMouseButtonUp (0)) {
+				egg.Drop();
+				_onetouch = true;
+			} 
+		}
+		if (Input.GetMouseButtonDown(0) && _clear) {
+
+			SceneManager.LoadScene("Stage2");
+		}
+	}
+
+	public void TkgSuccess() {
+		_clear = true;
+		_text.text = "Hit";
+
+	}
+
+		
+}
---
+++ Unity/TKGgame/Assets/Scripts/EggController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class EggController : MonoBehaviour {
+	public GameManager mng;
+	public Material _tkg;
+	void Awake () {
+		gameObject.GetComponent<Rigidbody>().useGravity = false;
+	}
+	public void SetPosition(Vector3 newPosition) {
+		gameObject.transform.position = newPosition;
+	}
+
+	public void Drop() {
+		gameObject.GetComponent<Rigidbody> ().useGravity = true;
+    }
+
+	void OnCollisionEnter(Collision col) {
+		if(col.gameObject.tag == "Dish"){
+			mng.TkgSuccess();
+
+			Destroy(this.gameObject);
+			col.gameObject.GetComponent<Renderer> ().material = _tkg;
+	}
+  }
+}

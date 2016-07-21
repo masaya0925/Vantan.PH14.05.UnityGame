8187d7d92d440903045035564962b0c130ae6e30
Leon Akasaka <leon@wisdomsoft.jp>
2016-07-21 11:27:32 +0900

    水平バーをプレハブ化、アニメーションを統一。

---
+++ Unity/TKGgame/Assets/Scripts/Bar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _leftBar = null;
+
+    [SerializeField]
+    private GameObject _rightBar = null;
+
+    [SerializeField]
+    private float delay = 0;
+
+    private IEnumerator InitializeIterator()
+    {
+        var leftAnimator = _leftBar.GetComponent<Animator>();
+        var rightAnimator = _rightBar.GetComponent<Animator>();
+
+        yield return new WaitForSeconds(delay);
+
+        leftAnimator.enabled = true;
+        rightAnimator.enabled = true;
+    }
+
+    void Awake()
+    {
+        StartCoroutine(InitializeIterator());
+    }
+}

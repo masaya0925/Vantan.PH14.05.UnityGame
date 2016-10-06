bd6fcd5259cf7f798d0838d9f52cda8aa0dbb148
masaya0925 <masaya_suda0925@i.softbank.jp>
2016-10-06 13:21:44 +0900

    project更新

---
+++ Game/Unity/New Game/Assets/Scripts/spin2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin2 : MonoBehaviour {
+
+	private int spin = 10;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	void OnCollisionStay(Collision col){
+		Debug.Log("atatta");
+
+		if(col.gameObject.name == "floar"){
+
+
+		}
+
+	}
+}
